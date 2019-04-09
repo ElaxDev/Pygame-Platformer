53fae5b7e59506ec6644aa6db15e939cbb77eec7
ElaxDev <31047609+ElaxDev@users.noreply.github.com>
2019-04-09 18:22:21 +0200

    Add files via upload

---
+++ platformer-pygame-master/var.py
@@ -0,0 +1,25 @@
+import pygame
+pygame.init()
+
+# Physics
+gravity = 9.8
+friction = 1.35
+pixel_per_meter = 10
+
+# Sizes
+window_width = 800
+window_height = 600
+player_size = [25, 50]
+
+# Positions
+player_coords = [20, 20]
+
+# Colors
+white = (255,255,255)
+gray = (200,200,200)
+red = (200,0,30)
+blue = (30,0,200)
+black = (0,0,55)
+
+# Game var
+vel = 20
